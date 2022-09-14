9b7158ebf8739a319c7180b8bcaf3a70cb6e13d3
OlhaShe <olha.hrebyniuk@gmail.com>
2022-09-14 23:23:01 +0300

    humidity, description, wind for searched city added

--- src/app.js
+++ src/app.js
@@ -44,23 +44,54 @@ setInterval(function () {
 
 // GET SEARCHED CITY
 
+function changeCityNameToSearchedOne(searchedCity) {
+  let changeCity = document.querySelector("#city");
+  let upperSearchedCity = searchedCity.toUpperCase();
+  changeCity.innerHTML = upperSearchedCity;
+}
+
+function showCityTemperature(response) {
+  let cityTemperature = Math.round(response.data.main.temp);
+  let selectTemp = document.querySelector("#current-temperature-value");
+  selectTemp.innerHTML = cityTemperature;
+}
+
+function showCityHumidity(response) {
+  let setHumidity = document.querySelector("#humidity");
+  let cityPrecipitation = response.data.main.humidity;
+  setHumidity.innerHTML = `HUMIDITY: ${cityPrecipitation}%`;
+  //  console.log();
+}
+
+function showCityWind(response) {
+  let setWind = document.querySelector("#wind");
+  let cityWind = Math.round(response.data.wind.speed);
+  setWind.innerHTML = `WIND: ${cityWind} km/h`;
+}
+
+function showCityWeatherDescription(response) {
+  let setDesc = document.querySelector("#day-description");
+  let updatedDesc = response.data.weather[0].description;
+  let updatedDescCaps = updatedDesc.toLocaleUpperCase();
+  setDesc.innerHTML = updatedDescCaps;
+}
+
 function getSearchedCityName(event) {
   event.preventDefault();
-  console.log("ghvghgv");
+
   let apiKey = `b40b135798f82a05aed08769f9275f50`;
+  let searchedCity = document.querySelector("#searchbar");
+  let unit = "metric";
+  let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity.value}&appid=${apiKey}&units=${unit}`;
 
-  let searchedCity = document.querySelector("searchbar");
-
-  let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=metric`;
-  console.log(apiLink);
+  changeCityNameToSearchedOne(searchedCity.value);
+  axios.get(apiLink).then(showCityTemperature);
+  axios.get(apiLink).then(showCityHumidity);
+  axios.get(apiLink).then(showCityWind);
+  axios.get(apiLink).then(showCityWeatherDescription);
 }
 
 let searchButton = document.querySelector("#site-search-button");
-if (searchButton) {
-  console.log("search button", searchButton);
-} else {
-  console.log("not button");
-}
 searchButton.addEventListener("click", getSearchedCityName);
 
 // var el = document.getElementById("#site-search-button");
